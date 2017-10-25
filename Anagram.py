e1ccc64aafdbab49ba9baf9e29792c1045ea0034
JenGoodridge <jen@jengoodridge.com>
2017-10-25 11:33:47 -0700

    update anagram.py
    updating comments and removing extra whitespace.

--- Anagram.py
+++ Anagram.py
@@ -1,18 +1,14 @@
 # This checks whether a string is an anagram of the other.
 from collections import Counter
 
-
 def check_for_anagram(s1, s2):
     return len(s1) == len(s2) and Counter(s1) == Counter(s2)
-
 
 def checkfor_anagram(string1, string2):
     if len(string1) != len(string2):
         return False
-
     newstring1 = sorted(string1)
     newstring2 = sorted(string2)
-
     return newstring1 == newstring2
 
 string1 = 'aaabbbc'
