1755cb1d5e06370f09385b6f0b3146e6b677848c
Jennifer Goodridge <jen@jengoodridge.com>
2018-02-01 10:41:13 -0800

    Adds unit tests

--- Nato_translation_Codewars.py
+++ Nato_translation_Codewars.py
@@ -1,4 +1,5 @@
 # to_nato takes a string and returns the NATO translation of the string.
+import unittest
 def to_nato(words):
     solution = []
     translation = ["Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliett", "Kilo", "Lima", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango", "Uniform", "Victor","Whiskey", "Xray", "Yankee", "Zulu"]
@@ -7,11 +8,13 @@ def to_nato(words):
             solution.append(translation[ord(char.lower()) - 97])
         else:
             solution.append(char)
-
     return ' '.join(solution)
 
-print(to_nato("The quick brown fox jumps over the lazy dog."))
-print(to_nato("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.!!!"))
-print(to_nato("aBc"))
-print(to_nato("xyZ"))
-
+class Test(unittest.TestCase):
+    def test_to_nato(self):
+        self.assertEqual(to_nato("The quick brown fox jumps over the lazy dog."), "Tango Hotel Echo   Quebec Uniform India Charlie Kilo   Bravo Romeo Oscar Whiskey November   Foxtrot Oscar Xray   Juliett Uniform Mike Papa Sierra   Oscar Victor Echo Romeo   Tango Hotel Echo   Lima Alfa Zulu Yankee   Delta Oscar Golf .")
+        self.assertEqual(to_nato("THE QUICK BROWN FOX! JUMPS OVER THE LAZY DOG?"), "Tango Hotel Echo   Quebec Uniform India Charlie Kilo   Bravo Romeo Oscar Whiskey November   Foxtrot Oscar Xray !   Juliett Uniform Mike Papa Sierra   Oscar Victor Echo Romeo   Tango Hotel Echo   Lima Alfa Zulu Yankee   Delta Oscar Golf ?")
+        self.assertEqual(to_nato("aBc"), "Alfa Bravo Charlie")
+        self.assertEqual(to_nato("xyZ"), "Xray Yankee Zulu")
+        
+unittest.main()
